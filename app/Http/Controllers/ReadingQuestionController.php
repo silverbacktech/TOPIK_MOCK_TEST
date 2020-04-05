2d4a6ab280c003214d872e5e9d9632c1e057ce8e
Samratp <58688155+samratpp@users.noreply.github.com>
2020-04-05 11:49:01 +0545

    Updated: Listening remains

--- app/Http/Controllers/ReadingQuestionController.php
+++ app/Http/Controllers/ReadingQuestionController.php
@@ -21,7 +21,7 @@ class ReadingQuestionController extends Controller
         $data=$request->all();
         // return $data;
         $i = 0;
-    
+        
         $questions=[];
         $options=[];
         $images=[];
@@ -34,7 +34,7 @@ class ReadingQuestionController extends Controller
         foreach ($data['question'] as $question) {
             $files=$request->file('questionfile');
             $name=$files[$i]->getClientOriginalName();
-            $store=$files[$i]->move(public_path().'\cover_img',$name);
+            $store=$files[$i]->move(public_path().'/cover_img',$name);
             array_push($questions, [
                 'id' => $question_id,
                 'question_group_id'=>$groupId,
